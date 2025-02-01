f9981ae3203095b767e55f819720d6468fd78790
vikasr <vikasr@winjit.com>
2025-02-01 17:22:22 +0530

    Added logic to check constructor called when object creates

--- Day1.py
+++ Day1.py
@@ -1,13 +1,16 @@
 class Employee:
     def __init__(self):
+        print("Inside the Initial Constructor")
         self.Name = "Vikas"
         self.Id = 123
         self.Dept  = "AI"
         self.Salary   = 100000
         self.Designation = "Senior Developer"
+        print("Inside at end of the attribute in Constructor")
 
 
     def Travel(self,destination):
+        print("Inside Travel function")
 
         print(f"Employee is now traving to the {destination}")
         
