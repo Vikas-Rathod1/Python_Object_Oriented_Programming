57cb037e621c4709be4e59b63b5c92da4e9bec42
vikasr <vikasr@winjit.com>
2025-02-01 17:10:52 +0530

    Accessing atributes using Class Objects

---
+++ Day1.py
@@ -0,0 +1,15 @@
+class Employee:
+    def __init__(self):
+        self.Name = "Vikas"
+        self.Id = 123
+        self.Dept  = "AI"
+        self.Salary   = 100000
+        self.Designation = "Senior Developer"
+
+
+
+
+
+emp = Employee()
+
+print(emp.Dept)
