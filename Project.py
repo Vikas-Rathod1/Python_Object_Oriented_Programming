5e16fa37bc5ed07c5e64be8b1be76d37781c72d9
vikasr <vikasr@winjit.com>
2025-02-14 21:47:22 +0530

    Added Experiments in jupyete note of Ops Methods

--- Project.py
+++ Project.py
@@ -12,10 +12,11 @@ class ChatBook:
                      2. Press 2 to signin
                      3. Press 3 to write a post
                      4. Press 4 to messsage a friend
-                     5. Presss any other key to Exit""")
+                     5. Presss any other key to Exit
+                \nThis is the place where you can press \n""")
         
         if user_input == "1":
-            pass
+            self.signup()
     
 
         elif user_input == "2":
@@ -30,5 +31,37 @@ class ChatBook:
         else: 
             exit()
 
+    def signup(self):
+        email = input("Enter you mail here -->")
+        passwrd = input("Enter your Password here -->")
+
+        self.username = email
+        self.password  = passwrd
+        print("You have signup successfully !!")
+        print("Thank you so much !!\n\n")
+        self.menu()
+
+    def signin(self):
+        if self.username=="" and self.password =="":
+            print("Please signup first by clicking 1 in the menu")
+        else:
+            usernam = input("Enter your email/username here -->")
+            passwrd = input("Enter your passwrod here -->")
+
+            if self.username==usernam and self.password==passwrd:
+                print("You have signed in successfully !!")
+                self.loggedin = True
+
+            else:
+                print("Please input correct Credentials...")
+            print("\n")
+            self.menu()
+
+
+
+    
+
 
 obj = ChatBook()
+
+#obj.signup()
